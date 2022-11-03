57533657bdb551923334df83a0b8e1d5259cc656
Abhijeet03s <abhijeetsharma476@gmail.com>
2022-11-03 15:02:03 +0530

    added icons in contact and skill section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,77 @@
-import React, { useRef } from 'react'
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Contact() {
-    const form = useRef();
-    const sendEmail = (e) => {
-        e.preventDefault();
-        emailjs.sendForm('service_yi5u7ak', 'template_z8p0c9v', form.current, 'nG2rb11fITDwilLBW')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-        e.target.reset();
-    }
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_yi5u7ak",
+        "template_z8p0c9v",
+        form.current,
+        "nG2rb11fITDwilLBW"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
 
-    return (
-        <>
-            <section id='contact' className='max-w-full flex flex-col justify-center items-center gap-5 p-10'>
-                <h1 className="text-[50px] pt-10 pb-5 font-Quicksand font-bold">Get In Touch</h1>
-                <h3 className="text-[30px]  font-Quicksand font-bold">Contact Me</h3>
-                <div className='w-[40%]'>
-                    <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-5 text-white'>
-                        <input className='w-full p-4 bg-transparent border border-[#3a36f3] border-solid resize-none' type="text" name='name' placeholder='Your Name' required />
-                        <input className='w-full p-4 bg-transparent border border-[#3a36f3] border-solid resize-none' type="email" name='email' placeholder='Your Email' required />
-                        <textarea className='w-full p-4 bg-transparent border border-[#3a36f3] border-solid resize-none' name="message" id="" rows="7" placeholder='Your Message...' required></textarea>
-                        <button className='btn btn-contact'>Send Message</button>
-                    </form>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section
+        id="contact"
+        className="max-w-full min-h-screen flex flex-col justify-center items-center gap-5 p-10"
+      >
+        <h1 className="text-[50px] pt-10 pb-5 font-Quicksand font-bold">
+          Get In Touch
+        </h1>
+        <div className="grid grid-cols-2 gap-x-10 w-[80%]">
+          <div className="flex justify-center items-center">
+            <img src="/assets/contact.png" alt="contact-img" />
+          </div>
+          <div className="w-[100%] space-y-5">
+            <h3 className="text-center text-[30px] font-Quicksand font-bold">
+              Contact Me
+            </h3>
+            <form
+              ref={form}
+              onSubmit={sendEmail}
+              className="flex flex-col gap-5 text-white"
+            >
+              <input
+                className="w-full p-4 bg-transparent border border-[#3a36f3] border-solid resize-none"
+                type="text"
+                name="name"
+                placeholder="Your Name"
+                required
+              />
+              <input
+                className="w-full p-4 bg-transparent border border-[#3a36f3] border-solid resize-none"
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                required
+              />
+              <textarea
+                className="w-full p-4 bg-transparent border border-[#3a36f3] border-solid resize-none"
+                name="message"
+                id=""
+                rows="7"
+                placeholder="Your Message..."
+                required
+              ></textarea>
+              <button className="btn btn-contact">Send Message</button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -4,39 +4,67 @@ export default function Skills() {
       <section id="skills" className="max-w-full bg-[#111111]">
         <div className="flex flex-col justify-center items-center px-20 pt-10 pb-20 space-y-12">
           <h1 className="text-[50px] font-Quicksand font-bold">Skills</h1>
-          <div className="flex flex-wrap justify-center items-center space-x-16 mb-[96px] text-center font-Pridi font-medium">
-            <article className="space-y-4">
-              <img className="w-24" src="/skills/HTML logo.svg" alt="html-logo" />
-              <h3>HTML</h3>
-            </article>
-            <article className="space-y-4">
-              <img className="w-24" src="/skills/CSS logo.svg" alt="css-logo" />
-              <h3>CSS</h3>
-            </article>
-            <article className="space-y-4">
-              <img className="w-24" src="/skills/JS logo.svg" alt="js-logo" />
-              <h3>JavaScript</h3>
-            </article>
-            <article className="space-y-4">
-              <img className="w-24" src="/skills/React logo.svg" alt="react-logo" />
-              <h3>React</h3>
-            </article>
-            <article className="space-y-4">
-              <img className="w-24" src="/skills/Tailwind logo.svg" alt="tailwind-logo" />
-              <h3>Tailwind</h3>
-            </article>
-            <article className="space-y-4">
-              <img className="w-24" src="/skills/Bootstrap logo.svg" alt="bootstrap-logo" />
-              <h3>Bootstrap</h3>
-            </article>
-            <div className="space-y-4">
-              <img className="w-24" src="/skills/Figma logo.svg" alt="figma-logo" />
-              <h3 className="text-center">Figma</h3>
+          <div className="grid grid-cols-2 gap-x-10 w-[80%]">
+            <div className="flex justify-center items-center">
+              <img src="/assets/skills.png" alt="skill-image" />
+            </div>
+            <div className="grid grid-cols-3 place-items-center gap-5 py-3 text-center font-Pridi font-medium">
+              <div className="space-y-4">
+                <img
+                  className="w-24"
+                  src="/skills/HTML logo.svg"
+                  alt="html-logo"
+                />
+                <h3>HTML</h3>
+              </div>
+              <div className="space-y-4">
+                <img
+                  className="w-24"
+                  src="/skills/CSS logo.svg"
+                  alt="css-logo"
+                />
+                <h3>CSS</h3>
+              </div>
+              <div className="space-y-4">
+                <img className="w-24" src="/skills/JS logo.svg" alt="js-logo" />
+                <h3>JavaScript</h3>
+              </div>
+              <div className="space-y-4">
+                <img
+                  className="w-24"
+                  src="/skills/React logo.svg"
+                  alt="react-logo"
+                />
+                <h3>React</h3>
+              </div>
+              <div className="space-y-4">
+                <img
+                  className="w-24"
+                  src="/skills/Tailwind logo.svg"
+                  alt="tailwind-logo"
+                />
+                <h3>Tailwind</h3>
+              </div>
+              <div className="space-y-4">
+                <img
+                  className="w-24"
+                  src="/skills/Bootstrap logo.svg"
+                  alt="bootstrap-logo"
+                />
+                <h3>Bootstrap</h3>
+              </div>
+              <div className="space-y-4">
+                <img
+                  className="w-24"
+                  src="/skills/Figma logo.svg"
+                  alt="figma-logo"
+                />
+                <h3 className="text-center">Figma</h3>
+              </div>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
-
