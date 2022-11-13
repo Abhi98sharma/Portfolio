2dea48b673db2a48cf186e2d77126947180642c6
Abhijeet98s <abhi.21smiley00@gmail.com>
2022-11-13 21:45:50 +0530

    fixed ui

---
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,75 @@
+import { Link } from "react-router-dom";
+
+export default function HamburgerMenu({ isOpen, toggle }) {
+  return (
+    <>
+      <nav
+        className={
+          isOpen
+            ? "bg-white w-full fixed inset-0 flex flex-col items-start justify-start gap-5 px-6 font-Inter text-white"
+            : "hidden"
+        }
+      >
+        <div className="container mx-auto w-full h-[80px] flex justify-between items-center bg-white">
+          <a href="#">
+            <img
+              className="w-40 lg:w-48 lg:h-20"
+              src="/assets/name-logo.svg"
+              alt="logo"
+            />
+          </a>
+
+          <svg
+            onClick={toggle}
+            className="w-8 h-8 hover:text-[#0C1821]"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </div>
+        <ul className="flex flex-col items-start gap-7">
+          <Link to="/">
+            <li
+              onClick={toggle}
+              className="text-lg text-white hover:text-[#0C1821] transition-colors"
+            >
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li
+              onClick={toggle}
+              className="text-lg text-white hover:text-[#0C1821] transition-colors"
+            >
+              About
+            </li>
+          </Link>
+          <Link to="/products">
+            <li
+              onClick={toggle}
+              className="text-lg text-white hover:text-[#0C1821] transition-colors"
+            >
+              Products
+            </li>
+          </Link>
+          <Link to="/contact">
+            <li
+              onClick={toggle}
+              className="text-lg text-white hover:text-[#0C1821] transition-colors"
+            >
+              Contact
+            </li>
+          </Link>
+        </ul>
+      </nav>
+    </>
+  );
+}
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,18 +12,22 @@ export default function Home() {
         </div>
         <div className="flex flex-col justify-center items-start space-y-5">
           <div className="flex items-end">
-            <h1 className="text-xl lg:text-2xl font-Kameron font-semibold">Abhijeet Sharma</h1>
+            <h1 className="text-xl lg:text-2xl font-Kameron font-semibold">
+              Abhijeet Sharma
+            </h1>
             <img
               className="mb-2 pl-1 lg:pl-2"
               src="/assets/Line 1.svg"
               alt="design-line"
             />
           </div>
-          <h1 className="text-[30px] lg:text-[60px] font-Quicksand name-heading">Front-end Developer</h1>
-          <p className="text-lg lg:text-[22px] lg:w-[80%] font-light font-Pridi">
-            I'm an front-end developer, fond of learning new technologies.
-            Passionate about designing and creating products. Currently,
-            learning Web Development.
+          <h1 className="text-[30px] lg:text-[60px] font-Quicksand name-heading">
+            Front-end Developer
+          </h1>
+          <p className="text-[17px] lg:text-[22px] lg:w-[80%] font-light font-Pridi">
+            I'm a front-end developer who's fond of learning new technologies.
+            passionate about designing and creating products. Currently, I am
+            learning Web development.
           </p>
           <div className="btn mt-10">
             <a
